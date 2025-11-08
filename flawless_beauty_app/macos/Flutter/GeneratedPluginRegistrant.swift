314c4eba43eb1e0c1429eb8cc8ad2e9193203219
Naduni Kodikara <naduchamako7@gmail.com>
2025-11-08 15:04:03 +0530

    Solved some bugs

--- flawless_beauty_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ flawless_beauty_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,13 +8,11 @@ import Foundation
 import app_links
 import path_provider_foundation
 import shared_preferences_foundation
-import tflite_flutter_helper
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AppLinksMacosPlugin.register(with: registry.registrar(forPlugin: "AppLinksMacosPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
-  TfliteFlutterHelperPlugin.register(with: registry.registrar(forPlugin: "TfliteFlutterHelperPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
--- flawless_beauty_app/windows/flutter/generated_plugins.cmake
+++ flawless_beauty_app/windows/flutter/generated_plugins.cmake
@@ -4,11 +4,11 @@
 
 list(APPEND FLUTTER_PLUGIN_LIST
   app_links
-  tflite_flutter_helper
   url_launcher_windows
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
+  tflite_flutter
 )
 
 set(PLUGIN_BUNDLED_LIBRARIES)
